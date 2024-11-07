27fa2b7d471e30dd2c1658e716c7f0b982d4686d
Kosuke Abe <s23c1003sm@s.chibakoudai.jp>
2024-11-07 12:48:14 +0900

    ALL test

--- test.bash
+++ test.bash
@@ -1,9 +1,7 @@
 #!/bin/bash -xv
-#SPDX-FileCopyrightText: 2024 Kosuke Abe
-#SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${}行目が違うよ
+	echo ${1}行目が違うよ
 	res=1
 }
 
@@ -12,13 +10,19 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-out=$(echo あ | ./plus)
-[ "$?" = 1]       || ng "$LINENO"
+　 
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)           
+[ "$?" = 1 ]      || ng "$LINENO" 
+[ "${out}" = "" ] || ng "$LINENO" 
+
+out=$(echo | ./plus)              
+[ "$?" = 1 ]      || ng "$LINENO" 
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
-[ "$?" = 1]       || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
+
 exit $res
+
+
