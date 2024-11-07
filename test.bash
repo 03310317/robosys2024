8b76326f8422052a7c8b627571fec67ce18611ae
Kosuke Abe <s23c1003sm@s.chibakoudai.jp>
2024-11-07 12:58:23 +0900

    ALL test

--- test.bash
+++ test.bash
@@ -10,14 +10,14 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-　 
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)           
-[ "$?" = 1 ]      || ng "$LINENO" 
-[ "${out}" = "" ] || ng "$LINENO" 
 
-out=$(echo | ./plus)              
-[ "$?" = 1 ]      || ng "$LINENO" 
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 
