21589f35002e6ca1253f07ad8325c0cbe2831daa
Kosuke Abe <s23c1003sm@s.chibakoudai.jp>
2024-11-29 15:51:01 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -8,7 +8,7 @@ ng () {
 
 res=0
 
-out=$(echo -e "太郎\n\n" | ./omikuzi)
+out=$(echo -e "太郎\n\n" | ./random)
 [ "$?" = 0 ] || ng "$LINENO"
 
 # 必要な出力を確認
@@ -16,14 +16,14 @@ echo "${out}" | grep -Eq "凶|末吉|半吉|吉|大吉" || ng "$LINENO"
 echo "${out}" | grep -q "太郎さんの運勢は..." || ng "$LINENO"
 
 # 異常入力テスト (名前が空の場合)
-out=$(echo -e "\n\n" | ./omikuzi 2>&1)
+out=$(echo -e "\n\n" | ./random 2>&1)
 [ "$?" = 0 ] || ng "$LINENO"
 echo "${out}" | grep -q "匿名さんの運勢は" || ng "$LINENO"
 
 # 予期しない入力の確認
-out=$(echo -e "あ\n\n" | ./omikuzi)
+out=$(echo -e "太郎\n\n" | ./random)
 [ "$?" = 0 ] || ng "$LINENO"
-echo "${out}" | grep -Eq "あさんの運勢は..." || ng "$LINENO"
+echo "${out}" | grep -Eq "太郎さんの運勢は..." || ng "$LINENO"
 
 # プログラム中にエラーが発生していないことを確認
 echo "${out}" | grep -Eq "Traceback|SyntaxError|EOFError" && ng "$LINENO"
