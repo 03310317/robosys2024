8db3bba30add259f4879475bf7d774784987f443
Kosuke Abe <s23c1003sm@s.chibakoudai.jp>
2024-12-07 16:15:50 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,5 +1,6 @@
 #!/bin/bash -xv
-# SPDX-FileCopyrightText: 2024 Kosuke Abe
+#! SPDX-FileCopyrightText: 2024 Kosuke Abe
+#! SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
     echo "${1}行目が違うよ"
@@ -8,7 +9,7 @@ ng () {
 
 res=0
 
-out=$(echo -e "太郎\n\n" | ./random)
+out=$(echo -e "太郎\n\n" | ./omikuji_simulator)
 [ "$?" = 0 ] || ng "$LINENO"
 
 # 必要な出力を確認
@@ -16,12 +17,12 @@ echo "${out}" | grep -Eq "凶|末吉|半吉|吉|大吉" || ng "$LINENO"
 echo "${out}" | grep -q "太郎さんの運勢は..." || ng "$LINENO"
 
 # 異常入力テスト (名前が空の場合)
-out=$(echo -e "\n\n" | ./random 2>&1)
+out=$(echo -e "\n\n" | ./omikuji_simulator 2>&1)
 [ "$?" = 0 ] || ng "$LINENO"
 echo "${out}" | grep -q "匿名さんの運勢は" || ng "$LINENO"
 
 # 予期しない入力の確認
-out=$(echo -e "太郎\n\n" | ./random)
+out=$(echo -e "太郎\n\n" | ./omikuji_simulator)
 [ "$?" = 0 ] || ng "$LINENO"
 echo "${out}" | grep -Eq "太郎さんの運勢は..." || ng "$LINENO"
 
