d9b509ea7d5d58828b74f9534a76a11633558462
Kosuke Abe <s23c1003sm@s.chibakoudai.jp>
2024-12-07 16:34:15 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -14,17 +14,12 @@ out=$(echo -e "太郎\n\n" | ./omikuji_simulator)
 
 # 必要な出力を確認
 echo "${out}" | grep -Eq "凶|末吉|半吉|吉|大吉" || ng "$LINENO"
-echo "${out}" | grep -q "太郎さんの運勢は..." || ng "$LINENO"
+echo "${out}" | grep -Eq "太郎さんの運勢は..." || ng "$LINENO"
 
 # 異常入力テスト (名前が空の場合)
 out=$(echo -e "\n\n" | ./omikuji_simulator 2>&1)
 [ "$?" = 0 ] || ng "$LINENO"
 echo "${out}" | grep -q "匿名さんの運勢は" || ng "$LINENO"
-
-# 予期しない入力の確認
-out=$(echo -e "太郎\n\n" | ./omikuji_simulator)
-[ "$?" = 0 ] || ng "$LINENO"
-echo "${out}" | grep -Eq "太郎さんの運勢は..." || ng "$LINENO"
 
 # プログラム中にエラーが発生していないことを確認
 echo "${out}" | grep -Eq "Traceback|SyntaxError|EOFError" && ng "$LINENO"
